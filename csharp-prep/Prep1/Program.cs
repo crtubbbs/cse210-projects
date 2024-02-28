0de5b59ffa17eef6ab4a6e05d7862115968974aa
crtubbs <crtubbs@byui.edu>
2024-02-27 21:46:38 -0700

    1 and 2 test

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,11 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
-}
+
+
+    Console.WriteLine("Enter first name: ");
+    string firstname = Console.ReadLine();
+
+    Console.WriteLine("Enter last name: ");
+    string lastname = Console.ReadLine();
+
+    Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Please provide your score.");
+        }
+
+        int score = Convert.ToInt32(Console.ReadLine());
+
+        char letter;
+
+        if (score >= 90)
+        {
+            letter = 'A';
+        }
+        else if (score >= 80)
+        {
+            letter = 'B';
+        }
+        else if (score >= 70)
+        {
+            letter = 'C';
+        }
+        else if (score >= 60)
+        {
+            letter = 'D';
+        }
+        else
+        {
+            letter = 'F';
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (score >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the course.");
+        }
     }
 }
