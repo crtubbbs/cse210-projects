be93b5f3ea3eb906df9acdff517d0b96cf02f839
crtubbs <crtubbs@byui.edu>
2024-03-04 15:40:50 -0700

    Learning Objective

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number?");
+        int magic = Convert.ToInt32(Console.ReadLine());
+
+        int count = 0;
+        int guess = -1;
+
+        while (magic != guess)
+        {
+            Console.WriteLine("What is your guess?");
+            guess = Convert.ToInt32(Console.ReadLine());
+            if (guess > magic)
+            {
+                Console.WriteLine("Lower!");
+            }
+
+            if (guess < magic)
+            {
+                Console.WriteLine("Higher!");
+            }
+            count++;
+        }
+
+        Console.WriteLine($"Congratulations! You guessed the magic number {magic}, in {count} attempts.");
+
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,44 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a series of numbers (enter 0 to stop):");
+        int sumofall = 0;
+
+        int largest = -999;
+
+        while (true)
+        {       
+            Console.WriteLine("Enter Number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number == 0)
+                break;
+
+            numbers.Add(number);
+
+            sumofall = number + sumofall;
+
+            if (number > largest)
+                largest = number;
+        }
+        
+        int numofitems = numbers.Count();
+
+        int average =  sumofall / numofitems;
+
+
+        Console.WriteLine($"The sum of these numbers is {sumofall}");
+        Console.WriteLine($"The average of the numbers is {average}");
+        Console.WriteLine($"The largest number is {largest}");
+        // Console.WriteLine("Numbers entered:");
+        // foreach (int num in numbers)
+        // {
+        //     Console.WriteLine(num);
+        // }
     }
 }
